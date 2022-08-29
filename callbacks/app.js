be8237aa450749dc9bbb92ad2f6561e4f58f78d1
taiyebnirjhar <nirjhar.212902003.green.ac.bd>
2022-08-29 19:34:44 +0600

    callbacks

---
+++ callbacks/app.js
@@ -0,0 +1,17 @@
+function myDisplayer(some) {
+  console.log(some);
+}
+
+function myCalculator(num1, num2, myCallback) {
+  let sum = num1 + num2;
+
+  if (myCallback) myCallback(sum);
+}
+
+myCalculator(5, 5, myDisplayer);
+
+// or
+
+myCalculator(5, 5, function (some) {
+  console.log(some);
+});
